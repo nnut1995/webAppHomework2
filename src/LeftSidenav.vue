1be2e9582aa8f411bd9d5659056f664204fe564d
nnut1995 <nnut1995@gmail.com>
2017-03-13 08:55:03 +0700

    initial setup

---
+++ src/LeftSidenav.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="left-sidenav">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title">Homework 2</h2>
+    </md-toolbar>
+    <md-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+    </md-sidenav>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// ---------------------
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+Vue.use(VueMaterial)
+
+import Axios from 'axios'
+Vue.$http = Axios
+// ---------------------
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
